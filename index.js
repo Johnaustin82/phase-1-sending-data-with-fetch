f3008d9977126de8a550eaa93fa997923b13c4fe
John Austin Ngode <austinngode23@gmail.com>
2024-04-15 18:56:37 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+    const data = {
+      name: name,
+      email: email
+    };
+  
+    const object = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(data)
+    };
+  
+    return fetch("http://localhost:3000/users", object)
+      .then(response => response.json())
+      .then(data => {
+        document.body.textContent = data.id;;
+      })
+      .catch(error => {
+        document.body.textContent= error.message;
+      });
+  }
+  
